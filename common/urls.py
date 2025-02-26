ead8b3cb20e4516c362a156bec5984d990c29471
Khudoykulov <diyorbekxudoyqulov627@gmail.com>
2025-02-26 20:41:37 +0500

    1

---
+++ common/urls.py
@@ -0,0 +1,30 @@
+from django.urls import path
+from .views import (
+    regions_list_view,
+    district_list_view,
+    ramadan_time,
+    region_detail,
+    categories_list,
+    prayer_times_api_by_region,
+    category_detail,
+    surah_list,
+    surah_detail,
+# monthly_prayer_times_api,
+prayer_times_api_by_district,
+today_ramadan_times
+)
+
+urlpatterns = [
+    path('regions/', regions_list_view, name='regions'),
+    path('region_detail/<int:pk>', region_detail, name='region_detail'),
+    path('districts/<int:pk>/', district_list_view, name='districts'),
+    path('ramadan_time/<int:d_id>', ramadan_time, name='ramadan_time'),
+    path('categories_list/', categories_list, name='categories_list'),
+    path('category_detail/<int:pk>/', category_detail, name='category_detail'),
+    path('surah_list/', surah_list, name='surah_list'),
+    path('surah_detail/<int:pk>', surah_detail, name='surah_detail'),
+    path('prayer_times_api/<int:region_id>', prayer_times_api_by_region, name='prayer_times_api'),
+    # path('monthly_prayer_times_api', monthly_prayer_times_api, name='monthly_prayer_times_api'),
+    path('prayer_times_api_by_district/<int:district_id>/', prayer_times_api_by_district, name='prayer_times_api_by_district'),
+    path('today_ramadan_times/<int:district_id>/', today_ramadan_times, name='today_ramadan_times'),
+]
--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView, )
+from rest_framework_simplejwt.views import TokenVerifyView
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from config import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    path(' ', SpectacularAPIView.as_view(), name='schema'),
+    # Optional UI:
+    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger'),
+    path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
     path('admin/', admin.site.urls),
+    path('common/', include('common.urls'), name='common')
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
